2a4785c1d4aa07aeb6c0b62c4cdaee333752a730
Navaneeth Gade <navaneethgade07@gmail.com>
2025-04-18 13:06:22 +0530

    Routes for dashboard

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -5,11 +5,15 @@ const authenticateToken = require('../middlewares/auth');
 const { 
   getFinancialOverview, 
   getRecentTransactions, 
-  getExpenseCategories 
+  getExpenseCategories,
+  getMonthlyAnalytics,
+  getProperties
 } = require('../controllers/dashboardController');
 
 router.get('/financial-overview', authenticateToken, getFinancialOverview);
 router.get('/recent-transactions', authenticateToken, getRecentTransactions);
 router.get('/expense-categories', authenticateToken, getExpenseCategories);
+router.get('/monthly-analytics', authenticateToken, getMonthlyAnalytics);
+router.get('/properties', authenticateToken, getProperties); 
 
 module.exports = router;
